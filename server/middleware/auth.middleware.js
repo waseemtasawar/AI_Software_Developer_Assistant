a842190d5e91bf5abb5afcfc962215e7a098790d
waseemtasawar <waseemtasawar96@gmail.com>
2026-07-06 15:14:04 +0500

    Add protected middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,43 @@
+const catchAsync = require('../utils/catchAsync');
+const appError = require('../utils/appError');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+const Apperror = require('../utils/appError');
+
+exports.protect = catchAsync(async (req, res, next) =>{
+    // 1) Getting token and check if it's there
+    let token;
+    if(req.headers.authorization && 
+        req.headers.authorization.startsWith("Bearer"))
+        {
+        token = req.headers.authorization.split(" ")[1];
+
+    } else if(req.cookies.jwt){
+        token = req.cookies.jwt;
+    }
+    if(!token){
+        new Apperror("You are not logged in! Please log in to get access", 401);
+    }
+     
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+
+    const currentUser = await User.findById(decoded.id);
+    if(!currentUser){
+        return next(
+            new Apperror("The user belonging to this token does no longer exist", 401)    
+        )
+    }
+    
+    // 4) Check if user changed password after the token was issued
+    if(currentUser.changedPasswordAfter(decoded.iat)){
+        return next(
+            new Apperror("User recently changed password! Please log in again", 401)
+        )
+    }
+    // Grant access to protected route
+    req.user = currentUser;
+    next();
+})
