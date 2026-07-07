b62ba70988be4de554587b0e9d869f201bd537ff
waseemtasawar <waseemtasawar96@gmail.com>
2026-07-07 15:45:53 +0500

    Add Project controller

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -50,3 +50,50 @@ const uploadProject = catchAsync(async (req, res, next) => {
     totalFiles: filesToSave.length,
   });
 });
+
+const getProjects = catchAsync(async (req,res, next) =>{
+    const projects = await Project.find({user:req.user._id}).sort({createdAt:-1});
+    return res.status(200).json({
+        success: true,
+        count: projects.length,
+        projects,
+    });
+});
+
+const getSingleProject = catchAsync(async (req, res, next) =>{
+    const project = await Project.findOne({
+        _id:req.params.projectId,
+        user:req.user._id
+    })
+    if(!project){
+        return next(new appError("Project not found", 404));
+    }
+    return res.status(200).json({
+        success: true,
+        project
+    });
+});
+
+const deleteProject = catchAsync(async (req,res, next) =>{
+    const project = await Project.findOne({
+        _id:req.params.projectId,
+        user:req.user._id
+    })
+    if(!project){
+        return next(new appError("Project not found", 404));
+    }
+    await File.deleteMany({project:project._id});
+    await Project.findByIdAndDelete(project._id);
+    return res.status(200).json({
+        success: true,
+        message: "Project deleted successfully"
+    });
+})
+
+
+module.exports ={
+    uploadProject,
+    getProjects,
+    getSingleProject,
+    deleteProject
+}
